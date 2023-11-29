5236ce52fb91cbf7c6722f7fca3ba62f8296cd32
joeyzazalin <josephanteyi@gmail.com>
2023-11-28 19:21:08 -0800

    first python push

---
+++ 0x00-python-hello_world/3-print_number.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+number = 98
+
+print(f"{number} Battery")
+print(f"{'-' * 10}")
+print(f"{number} Battery street")
---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+str1 = "Holberton"
+str2 = "School"
+
+print(f"Welcome to {str1}!")
+print(f"{str1} {str2}!")
