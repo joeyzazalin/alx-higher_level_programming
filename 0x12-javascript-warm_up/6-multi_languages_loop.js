2fd0be18aaa8a47e7ecc56ef77bed69ac4ccdc5a
joeyzazalin <josephanteyi@gmail.com>
2024-06-21 01:57:06 -0700

    javascript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-// script to print string using a loop
+
+// script to print strings using a loop
 
 let i;
-const myString = ['c is fun'; 'python is cool'; 'JavaScript is amazing'];
+const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
 for (i = 0; i < 3; i++) {
-	console.log(myString[i]);
+  console.log(myString[i]);
 }
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const x = math.floor(number(process.argv[2]));
+const x = Math.floor(Number(process.argv[2]));
 if (isNaN(x)) {
-	console.log('missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-	for (let i = 0; i < x; i++) {
-		console.log('C is fun');
-	}
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
