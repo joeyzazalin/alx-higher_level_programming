64b0aa4a1cb5718366fff0031a2d96cf83ebe4c8
joeyzazalin <josephanteyi@gmail.com>
2024-06-21 01:36:47 -0700

    javascript

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
-const myVar = 'C is fun\npython is cool\njavascript is amazing';
+
+// Script that prints C is fun,Python is cool and JavaScript is amazing.
+
+const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
 console.log(myVar);
